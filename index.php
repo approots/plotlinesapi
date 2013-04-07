e17519d652688781f2151b7bc6b0ff13aa766049
approots <rickjolly@hotmail.com>
2013-04-07 16:31:36 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//echo "Hello from inside!";
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+
+$app->run();
