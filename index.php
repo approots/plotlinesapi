d48f75f8a34876f198b4acb37cee03fc88dcbaf6
approots <rickjolly@hotmail.com>
2013-04-07 21:48:25 -0700

    aptana project

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
-//echo "Hello from inside!";
 
-require 'vendor/autoload.php';
+define('IS_PRODUCTION', ($_SERVER['SERVER_NAME'] != 'localhost') && ($_SERVER['SERVER_NAME'] != 'plotlinesapi');
+
+$baseDir = 'plotlinesapi/';
+if (IS_PRODUCTION)
+{
+    $baseDir = '../../plotlinesapi/';
+}
+
+require $baseDir . 'vendor/autoload.php';
+
 
 $app = new \Slim\Slim();
 
+require $baseDir . 'routes/test.php';
+/*
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
+*/
 
 $app->run();
